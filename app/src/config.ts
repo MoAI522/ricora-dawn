415af67f1e6a0ed7592491efa56eb13a71e749e7
MoAI522 <urotanre522@gmail.com>
2021-10-24 18:18:54 +0000

    デプロイ前調整

--- app/src/config.ts
+++ app/src/config.ts
@@ -1,7 +1,7 @@
 export default {
-  MUSIC_PATH: "/music/",
-  SKYBOX_IMAGE_PATH: "/images/skybox/",
-  LOGO_IMAGE_PATH: "/images/logo/",
-  SNS_LOGO_IMAGE_PATH: "/images/sns/",
+  MUSIC_PATH: "./music/",
+  SKYBOX_IMAGE_PATH: "./images/skybox/",
+  LOGO_IMAGE_PATH: "./images/logo/",
+  SNS_LOGO_IMAGE_PATH: "./images/sns/",
   DEBUG: false,
 };
